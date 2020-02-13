8e6e6d56f969d52646d48cfcea53f5ce99fde3e8
Jimmy <jimmy888swe@gmail.com>
2020-02-13 13:47:23 +0100

    feat: created basic rect, line, arc on canvas

---
+++ js.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+const c = canvas.getContext('2d');
+
+//rect
+
+c.fillStyle = 'green';
+c.fillRect(100, 100, 100, 100);
+c.fillStyle = '#eee';
+c.fillRect(300, 400, 100, 100);
+c.fillStyle = '#00efff';
+c.fillRect(300, 100, 100, 100);
+
+// Line
+
+c.beginPath();
+c.moveTo(50, 400);
+c.lineTo(300, 100);
+c.lineTo(400, 350);
+c.strokeStyle = '#ff0000';
+c.stroke();
+
+//arc
+
+c.beginPath();
+c.arc(300,300,30, 0, Math.PI * 2, false );
+c.strokeStyle = 'orange'
+c.stroke();
