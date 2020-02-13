4242a87677fe1fef2b9dbd4907a09ac761f70389
Jimmy <jimmy888swe@gmail.com>
2020-02-13 14:07:09 +0100

    feat: animation on arc

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
-const canvas = document.getElementById('canvas');
+ const canvas = document.getElementById('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 
-const c = canvas.getContext('2d');
-
+const c = canvas.getContext('2d'); 
+/* 
 //rect
 
 c.fillStyle = 'green';
@@ -31,11 +31,26 @@ c.arc(300,300,30, 0, Math.PI * 2, false );
 c.strokeStyle = 'orange'
 c.stroke();
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 25; i++) {
   let x = Math.random() * window.innerWidth;
   let y = Math.random() * window.innerHeight;
 c.beginPath();
 c.arc(x,y,50, 0, Math.PI * 2, false );
-c.strokeStyle = 'orange'
+c.strokeStyle = 'red';
 c.stroke();
 }
+ */
+
+let x = 200;
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0,0, innerWidth, innerHeight);
+  c.beginPath();
+  c.arc(x, 200, 30 ,0 , Math.PI * 2, false);
+  c.strokeStyle = 'green';
+  c.stroke();
+x +=1;
+  }
+
+
+ animate();
